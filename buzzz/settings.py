2f53a30aa459d3644ff3066f651ecf871b357964
Justin Smethers <justin.smethers@gmail.com>
2023-12-02 21:57:51 -0500

    Changed allowed_hosts for local development

--- buzzz/settings.py
+++ buzzz/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['localhost', 'justinsmethers.com']
+ALLOWED_HOSTS = ['justinsmethers.com', '127.0.0.1']
 
 
 # Application definition
